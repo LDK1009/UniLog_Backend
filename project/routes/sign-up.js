c1739a31307fc01cf3901db538a91f6463ac0f37
Soon_Coding <86905679+LDK1009@users.noreply.github.com>
2024-08-28 17:24:32 +0900

    ✨ feat : 회원가입 기능 구현

--- project/routes/sign-up.js
+++ project/routes/sign-up.js
@@ -1,12 +1,18 @@
-const express =  require('express');
+const express = require("express");
 const router = express.Router();
+const { User } = require("../models/index");
 
+// POST /sign-up
+router.post("/", async (req, res) => {
+  try {
+    await User.create(req.body);
 
-
-// POST /sign-up 
-router.post('/', (req, res, next)=>{
-    console.log(req.body);
-    res.json(req.body);
-})
+    console.log("회원가입 성공 ");
+    res.status(201).send("회원가입 성공");
+  } catch (error) {
+    console.error("회원가입 실패:", error); // 오류 로그 출력
+    res.status(500).send("회원가입 실패");
+  }
+});
 
 module.exports = router;
