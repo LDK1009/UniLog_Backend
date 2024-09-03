e9b1e982365a2218fe51867225e3468bacdffade
Soon_Coding <86905679+LDK1009@users.noreply.github.com>
2024-09-03 13:43:56 +0900

    🔨 refactor : 비동기 오류처리 모듈 분리

--- project/controller/users/userController.js
+++ project/controller/users/userController.js
@@ -2,28 +2,24 @@ const { User } = require("../../models/index");
 const wrapAsyncController = require("../../utills/wrapAsyncController");
 
 exports.getAllUsers = wrapAsyncController(async (req, res) => {
-    const datas = await User.findAll();
-    res.status(201).json(datas);
+  const datas = await User.findAll();
+  res.status(201).json(datas);
 });
 
-exports.checkDuplicateUser = async (req, res) => {
-  try {
-    const { uid } = req.body;
-    const datas = await User.findAll({
-      attributes: ["uid"],
-      where: { uid: uid },
-    });
-    const users = datas.map((el) => el.dataValues.uid);
+exports.checkDuplicateUser = wrapAsyncController(async (req, res) => {
+  const { uid } = req.body;
+  const datas = await User.findAll({
+    attributes: ["uid"],
+    where: { uid: uid },
+  });
+  const users = datas.map((el) => el.dataValues.uid);
 
-    if (users.length < 1) {
-      res.json(true);
-    } else {
-      res.json(false);
-    }
-  } catch (error) {
-    res.json(`에러 발생! ${error}`);
+  if (users.length < 1) {
+    res.json(true);
+  } else {
+    res.json(false);
   }
-};
+});
 
 exports.createUser = (req, res) => {
   res.json("사용자 생성");
