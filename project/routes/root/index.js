531904160b1e2b26bec4ebeea97f5e2115a26d05
Soon_Coding <86905679+LDK1009@users.noreply.github.com>
2024-09-05 11:04:03 +0900

    📝 feat : HTML 필터링 기능 추가(HTML 전송 시 악성코드 제거)

--- project/routes/root/index.js
+++ project/routes/root/index.js
@@ -3,6 +3,8 @@ const router = express.Router();
 const fs = require("fs");
 const path = require("path");
 const { marked } = require("marked");
+const { JSDOM } = require("jsdom");
+const DOMPurify = require("dompurify")(new JSDOM().window); // jsdom을 통해 DOM 환경 설정
 
 // 회원가입 라우트
 router.get("/", (req, res) => {
@@ -13,7 +15,8 @@ router.get("/", (req, res) => {
       return res.status(404).send("Post not found");
     }
     const htmlContent = marked(content);
-    res.send(htmlContent);  // res.json 대신 res.send 사용
+    const safeHTML = DOMPurify.sanitize(htmlContent); // 악성 코드 필터링
+    res.send(safeHTML); // res.json 대신 res.send 사용
   });
 });
 
